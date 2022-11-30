12181cc02edad20440df4c7491e8c149d1329375
misha <milisov4@gmal.com>
2022-12-01 00:01:13 +0200

    refactor javascript

--- app/javascript/add_fields_to_answer.js
+++ app/javascript/add_fields_to_answer.js
@@ -1,5 +1,3 @@
-$(document).ready(function() {
-  $('#add-file-to-answer-button').click(function() {
+$(document).on('click', '#add-file-to-answer-button', function() {
     $('.file-fields').append('<div class="mb-3"><input multiple="multiple" class="form-control" type="file" name="answer[files][]" id="answer_files"></input></div');
   });
-});
