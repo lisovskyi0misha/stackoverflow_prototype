1dd4ad161352b7b5b4b8c31ced2e1b42596d726a
misha <milisov4@gmal.com>
2022-12-19 12:18:42 +0200

    tests for twitter authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,14 +10,31 @@ class User < ApplicationRecord
   has_many :providers
 
   def self.from_omniauth(auth)
-    find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      # user.name = auth.info.name   # assuming the user model has a name
-      # user.image = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails, 
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
+    provider_params = { provider_name: auth.provider, uid: auth.uid }
+    if provider = Provider.find_by(provider_params)
+      user = provider.user
+    else
+      user = user_exist?(auth, provider_params)
+      return user if user
+      create_new_user(auth, provider_params)
     end
+  end
+
+  private
+
+  def self.create_new_user(auth, provider_params)
+    user = User.create!(email: auth.info.email, password: Devise.friendly_token[0, 20])
+    create_new_provider(user, provider_params)
+    user
+  end
+
+  def self.user_exist?(auth, provider_params)
+    user = User.find_by(email: auth.info.email)
+    create_new_provider(user, provider_params) if user
+    user
+  end
+
+  def self.create_new_provider(user, provider_params)
+    user.providers.create(provider_params)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,8 +10,44 @@ RSpec.describe User do
   it { should have_many :providers }
 
   describe '.from_omniauth' do
-    context 'user fave already been authorized'
-    
-    context 'user haven`t been authorized'
+    let!(:user) { create(:user) }
+    let(:auth) { OmniAuth::AuthHash.new(provider: 'github', uid: '12345', info: { email: 'test@test' }) }
+    let(:auth_for_existed_user) { OmniAuth::AuthHash.new(provider: 'github', uid: '12345', info: { email: user.email }) }
+
+    context 'user fave already been authorized' do
+      it 'returns user' do
+        user.providers.create(uid: '12345', provider_name: 'github')
+        expect(User.from_omniauth(auth)).to eq(user)
+      end
+    end
+
+    context 'user haven`t been authorized' do
+      context 'user exists' do
+        it 'doesn`t save user to db' do
+
+          expect { User.from_omniauth(auth_for_existed_user) }.to_not change(User, :count)
+        end
+
+        it 'creates new provider' do
+          expect { User.from_omniauth(auth_for_existed_user) }.to change(user.providers, :count).by(1)
+        end
+
+        it 'returns user' do
+          expect(User.from_omniauth(auth_for_existed_user)).to eq(user)
+        end
+      end
+
+      context 'user does not exist' do
+        it 'saves user to db' do
+          expect { User.from_omniauth(auth) }.to change(User, :count).by(1)       
+        end
+
+        it 'returns user' do
+          user1 = User.from_omniauth(auth)
+          expect(user1.email).to eq('test@test')
+          expect(user1.providers.first.provider_name).to eq('github')
+        end
+      end
+    end
   end
 end
