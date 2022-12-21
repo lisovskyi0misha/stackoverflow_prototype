402fe5b5ce3af3873ac40c3ecb0194dfcc4020d8
misha <milisov4@gmal.com>
2022-12-21 18:33:26 +0200

    installed doorkeeper and created files

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'devise'
 
 gem 'bootstrap_form', '~> 5.1'
 
+gem 'doorkeeper'
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  use_doorkeeper
   devise_for :users, controllers: { sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'questions#index'
