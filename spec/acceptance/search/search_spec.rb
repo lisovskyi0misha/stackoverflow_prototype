f5e1267ed13677a740c2dacd02b301181e3d617d
misha <milisov4@gmal.com>
2022-12-30 18:40:32 +0200

    global search feature level

--- spec/acceptance/search/search_spec.rb
+++ spec/acceptance/search/search_spec.rb
@@ -1,11 +1,28 @@
-require_relative '../acceptacne_helper'
+require_relative '../acceptance_helper'
 
 feature 'search' do
   let!(:user) { create(:user, email: 'ransack@email') }
-  scenario 'user`s search' do
-    visit search_path
-    fill_in 'Search', with: 'ransack'
-    click_on 'Search'
+  let!(:question) { create(:just_question, title: 'ransack test') }
+  let!(:answer) { create(:just_answer, body: 'ransack test body') }
+  let!(:comment) { create(:answers_comment, body: 'ransack comment test body') }
+
+  scenario 'users search' do
+    search
     expect(page).to have_content(user.email)
+  end
+
+  scenario 'questions search' do
+    search
+    expect(page).to have_content(question.title)
+  end
+
+  scenario 'answers search' do
+    search
+    expect(page).to have_content(answer.body)
+  end
+
+  scenario 'comments search' do
+    search
+    expect(page).to have_content(comment.body)
   end
 end
