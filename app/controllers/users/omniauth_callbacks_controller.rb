13e0e39b0e9b11c6e29bc265a9b6e39b35fa8739
misha <milisov4@gmal.com>
2023-01-12 14:52:28 +0200

    added twitter settings

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,22 +1,28 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user = User.from_omniauth(request.env['omniauth.auth'])
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: "GitHub") if is_navigational_format?
+      set_flash_message(:notice, :success, kind: 'GitHub') if is_navigational_format?
     else
-      session["devise.github_data"] = request.env["omniauth.auth"].except(:extra)
+      session['devise.github_data'] = request.env['omniauth.auth'].except(:extra)
       redirect_to new_user_registration_url
     end
   end
 
+  def passthru
+    binding.break
+    super
+  end
+
   def twitter2
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    binding.break
+    @user = User.from_omniauth(request.env['omniauth.auth'])
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: "Twitter2") if is_navigational_format?
+      set_flash_message(:notice, :success, kind: 'Twitter2') if is_navigational_format?
     else
-      session["devise.twitter2_data"] = request.env["omniauth.auth"].except(:extra)
+      session['devise.twitter2_data'] = request.env['omniauth.auth'].except(:extra)
       redirect_to new_user_registration_url
     end
   end
